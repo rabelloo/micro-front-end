a94d4a59691108a3d38b1802e4936bca579ee3f2
Andre Rabello <andre.luiz_r@hotmail.com>
2020-09-28 15:59:02 +0100

    chore(vue): use version

--- packages/vue/src/App.vue
+++ packages/vue/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Vue 3</h1>
+  <h1>Vue {{ version }}</h1>
   Current name is <code>{{ name }}</code>
   <label>
     Type to change name
@@ -11,11 +11,11 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, version } from 'vue';
 
 export default defineComponent({
   data() {
-    return { count: 0 };
+    return { count: 0, version };
   },
   props: {
     name: String,
